05d326bc0fd2138003117522a93a93b4779256e6
Francis <fwang36@gmail.com>
2022-02-16 16:19:25 -0500

    added movement controls

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function () {
+const { stderr, stdin } = require("process");
+
+let connection;
+const setupInput = function (conn) {
+  connection = conn
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -7,10 +11,23 @@ const setupInput = function () {
   return stdin;
 };
 
-const handleUserInput = function () {
-  if ('\u0003') {
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
     process.exit();
   }
-};
+  if (key === 'w') {
+    connection.write("Move: up")
+  }
+  if (key === 's') {
+    connection.write("Move: down")
+  }
+  if (key === 'a') {
+    connection.write("Move: left")
+  }
+  if (key === 'd') {
+    connection.write("Move: right")
+  }
+}  
+
 
 module.exports = { setupInput }
